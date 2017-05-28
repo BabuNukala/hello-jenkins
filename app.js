cfab8b3585ed6332c9838aa5fe921005254a326e
Babu <babu.nukala@gmail.com>
2017-05-28 13:41:36 +0200

    Adding index.html

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
- 
 var app = express();
- 
+var path = require('path');
+
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.sendfile(path.join(__dirname + '/index.html'));
 });
- 
+
 app.listen(process.env.PORT || 5000);
- 
+
 module.exports = app;
